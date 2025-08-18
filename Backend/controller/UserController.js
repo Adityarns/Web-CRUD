9709e496ce6a40e3bee24611d5005df248986d05
Aditya Rahman Syach <adityarachmansyach@gmail.com>
2025-08-18 12:52:13 +0700

    Menambahkan fitur update dan delete pada backend

--- Backend/controller/UserController.js
+++ Backend/controller/UserController.js
@@ -30,3 +30,24 @@ export const saveUser = async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 };
+
+export const updateUser = async (req, res) => {
+  try {
+    const updateduser = await User.updateOne(
+      { _id: req.params.id },
+      { $set: req.body }
+    );
+    res.status(200).json(updateduser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    const deleteduser = await User.deleteOne({ _id: req.params.id });
+    res.status(200).json(deleteduser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
